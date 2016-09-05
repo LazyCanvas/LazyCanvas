cebcbcd95ab95167afd2624f94ccfa92b113b46c
pwener <phwener@gmail.com>
2016-09-05 09:17:06 -0300

    Second refactor scope stack

--- include/structure/scope.h
+++ include/structure/scope.h
@@ -1,96 +1,39 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "objects.h"
+/**
+ * This file describe type and functions about stack used to
+ * work with scopes. Each scope created is stored with one id
+ * that follows the pattern <father_id><self_id>. That is useful
+ * to manage variable scope by example.
+ */
+#ifndef SCOPE_STACK_H
 
-typedef struct node {
-  struct node *next;
-  struct node *previous;
-  char *id;
-  char *name;
-  void *structure;
-} Node;
+#define SCOPE_STACK_H
 
+// The stack have many scopes identified by your id.
+typedef struct scope {
+  struct scope *next;
+  char *scope_id;
+} Scope;
+
+// Each new scope has added like head of stack
 typedef struct stack {
-  Node *head;
+  struct scope *head;
 } Stack;
 
-Stack *stack;
+// All operations are used through this pointer
+Stack *scope_stack;
 
-int create_scope(void);
-Node* create_scope_node(char*);
+//Below are describe some functions of stack structure.
 
-int remove_scope(void);
+int init_scopes(void);
 
-Node* search_scope(char*);
+Scope* create_scope_node(char *id);
 
-void print_scope(Node*);
+int pop(void);
 
-char* calculate_id(char*);
+char* calculate_id(char *last_scope_id);
 
-char*get_last_scope_id(void);
+void print_scopes(Scope*);
 
-int create_scope(void) {
-  char *id;
-  if(stack == NULL) {
-    stack = (Stack*) malloc(sizeof(Stack));
-    id = "1";
-    stack->head = create_scope_node(id);
-  } else {
-    char *id_last = stack->head->id;
-    char *new_id = calculate_id(id_last);
-    Node *node = create_scope_node(new_id);
-    node->next = stack->head;
-    stack->head = node;
-  }
+char* get_last_scope_id(void);
 
-  return 0;
-}
-
-char* get_last_scope_id(void) {
-  if(stack == NULL || stack->head == NULL) {
-    printf("Scope head cannot be null\n");
-    exit(0);
-  } else {
-    return stack->head->id;
-  }
-}
-
-Node* create_scope_node(char *id) {
-  Node *node = (Node*) malloc(sizeof(Node));
-
-  node->next = NULL;
-  node->previous = NULL;
-  node->id = id;
-
-  return node;
-}
-
-
-int remove_scope(void) {
-  Node *last = stack->head;
-  if(last != NULL) {
-    stack->head = last->next;
-    free(last);
-  } else {
-    exit(4);
-  }
-  return 1;
-}
-
-void print_scope(Node *node) {
-  if(node == NULL) {
-    return;
-  } else {
-    printf("%s\n", node->id);
-    print_scope(node->next);
-  }
-}
-
-char* calculate_id(char* id_previous) {
-  char *add_id = "-1";
-  char *new_id = (char*) malloc(strlen(id_previous) + strlen(add_id) + 1);
-  strcpy(new_id, id_previous);
-  strcat(new_id, add_id);
-  return new_id;
-}
+#endif
--- include/teste.c
+++ include/teste.c
@@ -1,10 +1,12 @@
 #include "figure/instances.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-  create_scope();
+  init_scopes();
   instance_line(0.0, 0.0, 10.0, 10.0, 1, "red", "circle");
-  create_scope();
+  init_scopes();
 
   instance_line(0.0, 0.0, 10.0, 10.0, 1, "green", "square");
   print_line(search_element("circle")->structure);
