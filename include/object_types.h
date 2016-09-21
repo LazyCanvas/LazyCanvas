e36db813b381e6cb1b62be6730e0969ac88bad76
pwener <phwener@gmail.com>
2016-09-21 09:46:02 -0300

    Instantiating drawn objects

---
+++ include/object_types.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_TYPES
+
+#define OBJECT_TYPES
+
+const char *CIRCLE_NAME = "Circle";
+const char *ELIPSE_NAME = "Elipse";
+const char *RECTANGLE_NAME = "Rectangle";
+const char *LINE_NAME = "Line";
+const char *ARC_NAME = "Arc";
+
+#endif
