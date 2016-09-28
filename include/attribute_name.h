b30a2c88e547c46ce4fd6765d08a0c173e86d2dd
kairala <kairala>
2016-09-28 09:44:04 -0300

    Included decision on instances atribution
    	Signed-off-by: Thiago Kairala <thiagor.kairala@gmail.com>
    	Signed-off-by: Ricardo Teixeira <ricardocdzg@hotmail.com>

---
+++ include/attribute_name.h
@@ -0,0 +1,18 @@
+#ifndef ATTRIBUTE_NAMES
+
+#define ATTRIBUTE_NAMES
+
+  const char* BACKGROUND = "background";
+  const char* LINE_WIDTH = "lineWidth";
+  const char* X = "x";
+  const char* Y = "y";
+  const char* RADIUS = "radius";
+  const char* START_ANGLE = "startAngle";
+  const char* FINAL_ANGLE = "finalAngle";
+  const char* FINAL_X = "finalX";
+  const char* FINAL_Y = "finalY";
+  const char* WIDTH = "width";
+  const char* HEIGTH = "heigth";
+  const char* CENTER_X = "centerX";
+  const char* CENTER_Y = "centerY";
+#endif
